440f2953428351dbba28941987abb57773758c26
Miguel Leite <miguelleite200leite@gmail.com>
2025-12-22 20:53:13 +0100

    feat: add new images and enhance Home component layout with social features

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
-import { UserRound } from "lucide-react";
+import { MessagesSquare, Share2, UserRound, Users } from "lucide-react";
 
 import Logo from "@/app/assets/eg-positivo.png";
 import Mockups from "@/app/assets/mockups.png";
+import Img01 from "@/app/assets/img01.png"
+import Img02 from "@/app/assets/img02.png"
 
 import FacebookGaming from "@/app/assets/icons/facebook gaming.svg"
 import Twitch from "@/app/assets/icons/twitch.svg"
@@ -69,6 +71,40 @@ export default function Home() {
             </div>
           </div>
         </main>
+      </div>
+
+      <div className="container py-16">
+        <div className="flex items-center justify-around mb-44">
+          <div>
+            <Image src={Img01} alt="Imagem 01" />
+          </div>
+          <div className="space-y-5">
+            <Share2 size={36} />
+            <div className="w-110.25 space-y-8">
+              <h3 className="text-4xl font-bold text-white leading-11">Uma rede social completa só de gamers</h3>
+              <p className="text-zinc-500 text-lg">Uma experiência 100% voltada para jogos. Aqui você pode postar sobre suas partidas, acompanhar seu streamer favorito e muito mais.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-around">
+          <div className="space-y-12">
+            <div className="space-y-5">
+              <Users size={36} />
+              <div className="w-110.25 space-y-8">
+                <h3 className="text-4xl font-bold text-white leading-11">Faça amigos para sua próxima partida</h3>
+                <p className="text-zinc-500 text-lg">Junte-se em grupos criados por usuários da nossa comunidade e nunca mais jogue sozinho!</p>
+              </div>
+            </div>
+            <div className="py-2 px-5 rounded-full bg-zinc-800 inline-flex items-center gap-2.5">
+              <MessagesSquare />
+              <p>Grupos</p>
+            </div>
+          </div>
+          <div>
+            <Image src={Img02} alt="Imagem 02" />
+          </div>
+        </div>
       </div>
     </div>
   );
